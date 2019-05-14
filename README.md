67f7e13bd3755d96462994e319fd5a40e0f6a927
Henrik Littke <henrik.littke@gmail.com>
2019-05-14 19:51:55 +0200

    Test grid on Homepage

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Laravel CMS
 ### Installation
 1. ´vagrant up´
 2. ´composer install´
-3. ´vagrant ssh && cd code/´
-4. ´php artisan migrate:refresh --seed´
-5. ´yarn install && yarn run watch´
-6. ´http://lighthouse.local´
+3. ´vagrant ssh´
+4. ´cd code/´
+5. ´php artisan migrate:refresh --seed´
+6. ´yarn install && yarn run watch´
+7. ´http://lighthouse.local´
--- resources/js/views/public/Home.jsx
+++ resources/js/views/public/Home.jsx
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import {
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 
 export default class Home extends Component {
   render() {
     return (
-      <h3>
-        Home!
-      </h3>
+      <div style={{
+          maxWidth: '1280px',
+          margin: 'auto',
+          padding: '20px',
+          backgroundColor: '#ffffff',
+        }}>
+        <Grid
+          container
+          spacing={40}>
+          <Grid item lg={12}>
+            <Typography variant="h2" style={{ marginBottom: '20px' }}>
+              Home
+            </Typography>
+            <Typography paragraph variant="body1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+              facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+              gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+              donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+              adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+              Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+              imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+              arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+              donec massa sapien faucibus et molestie ac.
+            </Typography>
+            <Typography paragraph variant="body1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+              facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+              gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+              donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+              adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+              Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+              imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+              arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+              donec massa sapien faucibus et molestie ac.
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
