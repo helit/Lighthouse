94262bed95e13d47eb7314a5c92df4b0a8345168
Henrik Littke <henrik@itiden.se>
2019-05-23 13:32:33 +0200

    Add menu listing (WIP)

--- database/seeds/MenusTableSeeder.php
+++ database/seeds/MenusTableSeeder.php
@@ -12,14 +12,44 @@ class MenusTableSeeder extends Seeder
     public function run()
     {
         DB::table('menus')->insert([
-            'title' => 'Menu',
+            'title' => 'Menu item 1',
             'parent_id' => 0,
             'visibility' => 'public'
         ]);
 
         DB::table('menus')->insert([
-            'title' => 'Sub Menu',
+            'title' => 'Menu item 2',
+            'parent_id' => 0,
+            'visibility' => 'public'
+        ]);
+
+        DB::table('menus')->insert([
+            'title' => 'Menu item 3',
+            'parent_id' => 0,
+            'visibility' => 'public'
+        ]);
+
+        DB::table('menus')->insert([
+            'title' => 'Sub Menu item 1',
             'parent_id' => 1,
+            'visibility' => 'public'
+        ]);
+
+        DB::table('menus')->insert([
+            'title' => 'Sub Menu item 1',
+            'parent_id' => 3,
+            'visibility' => 'public'
+        ]);
+
+        DB::table('menus')->insert([
+            'title' => 'Sub Menu item 2',
+            'parent_id' => 3,
+            'visibility' => 'public'
+        ]);
+
+        DB::table('menus')->insert([
+            'title' => 'Sub Menu item 3',
+            'parent_id' => 3,
             'visibility' => 'public'
         ]);
     }
--- resources/js/views/private/views/Menus.tsx
+++ resources/js/views/private/views/Menus.tsx
@@ -1,16 +1,117 @@
 import * as React from 'react';
+import api from '../../../services/api';
+import AdminPageLoader from '../../../components/AdminPageLoader';
 import {
-  Typography
+  Paper,
+  List,
+  Grid,
+  ListItem,
+  ListItemIcon,
+  Icon,
+  ListItemText,
+  Collapse
 } from '@material-ui/core';
 
 export default class Menus extends React.Component<any, any> {
+  state = {
+    menus: [],
+    isLoading: null,
+    open: true,
+  }
+
+  handleClick = () => {
+    this.setState(state => ({ open: !state.open }));
+  };
+
+  getMenus = () => {
+    let menuData = [];
+    this.setState({ isLoading: true });
+
+    api.get('/menus')
+      .then(response => {
+        const data = response.data.data;
+
+        data.map((menuItem, index) => {
+          if (menuItem.id > 0) {
+
+            if (menuData[menuItem.parentId] === undefined) {
+              menuData[menuItem.parentId] = [];
+              menuData[menuItem.parentId].push(menuItem);
+            } else {
+              menuData[menuItem.parentId].push(menuItem);
+            }
+          } else {
+            menuData.push();
+          }
+        });
+
+        console.log(menuData);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .then(() => {
+        this.setState({
+          isLoading: false,
+          menus: menuData
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.getMenus();
+  }
+
   render() {
+    const { isLoading } = this.state;
+
+    if (isLoading) {
+      return <AdminPageLoader />;
+    }
+
     return (
-      <div>
-        <Typography variant="h5" style={{ marginBottom: '20px' }}>
-          Menus
-        </Typography>
-      </div>
+      <React.Fragment>
+        <Grid container spacing={24} style={{ height: '100%' }}>
+          <Grid item xs={6}>
+            <Paper
+              elevation={1}
+              square={true}
+            >
+              <List>
+                <ListItem button onClick={this.handleClick}>
+                  <ListItemIcon>
+                    <Icon>inbox</Icon>
+                  </ListItemIcon>
+                  <ListItemText inset primary="Inbox" />
+                  {this.state.open
+                    ? <Icon>expand_less</Icon>
+                    : <Icon>expand_more</Icon>
+                  }
+                </ListItem>
+                <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                  <List disablePadding>
+                    <ListItem button style={{ paddingLeft: '32px' }}>
+                      <ListItemIcon>
+                        <Icon>star_border</Icon>
+                      </ListItemIcon>
+                      <ListItemText inset primary="Starred" />
+                    </ListItem>
+                  </List>
+                </Collapse>
+              </List>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper
+              elevation={1}
+              square={true}
+              style={{ padding: '24px' }}
+            >
+              Other stuff
+            </Paper>
+          </Grid>
+        </Grid>
+      </React.Fragment>
     );
   }
 }
